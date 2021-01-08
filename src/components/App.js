3b12fa3c4bd0bca7b4383156c1cce70c208b97dd
meghanapulavarthy <68858341+meghanapulavarthy@users.noreply.github.com>
2021-01-08 23:39:02 +0530

    answer

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,34 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state ={
-			isTrue:false
-		}
-	};
+    this.state = {
+      isTrue: false
+    };
+  }
 
-	handleClick = () => {
-		this.setState({isTrue:true});
-	}
+  handleClick = () => {
+    this.setState({ isTrue: true });
+  };
 
-
-    render() {
-    	return(
-    		<div id="main">
-				<button id="click" onClick={handleClick} >button</button>
-				{ this.state.isTrue ? <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> }
-    		</div>
-    	);
-    }
+  render() {
+    return (
+      <div id="main">
+        <button id="click" onClick={this.handleClick}>
+          button
+        </button>
+        {this.state.isTrue ? (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        ) : null}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
