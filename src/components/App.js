6c81454f8ad190df6682534d888d6cbfb5103007
meghanapulavarthy <meghana.pulavarthy@gmail.com>
2021-01-08 23:22:06 +0530

    master

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,22 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+
+		this.state ={
+			isTrue:false
+		}
 	};
+
+	handleClick = () => {
+		this.setState({isTrue:true});
+	}
+
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				<button id="click" onClick={handleClick} >button</button>
+				{ this.state.isTrue ? <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> }
     		</div>
     	);
     }
